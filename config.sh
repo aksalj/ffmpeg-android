795df29738fffe2ae2910648152c4022c34a5e35
Salama A.B. <devaksal@gmail.com>
2013-07-12 15:44:55 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -49,14 +49,4 @@ MP3LAME=../libmp3lame
  --enable-nonfree \
  ${ADDITIONAL_CONFIGURE_FLAG}
 
-sed -i "s/HAVE_SYMVER 1/HAVE_SYMVER 0/g" config.h
-sed -i "s/HAVE_SYMVER_GNU_ASM 1/HAVE_SYMVER_GNU_ASM 0/g" config.h
-sed -i "s/#define restrict restrict/#define restrict/g" config.h
-sed -i 's/HAVE_LRINT 0/HAVE_LRINT 1/g' config.h
-sed -i 's/HAVE_LRINTF 0/HAVE_LRINTF 1/g' config.h
-sed -i 's/HAVE_ROUND 0/HAVE_ROUND 1/g' config.h
-sed -i 's/HAVE_ROUNDF 0/HAVE_ROUNDF 1/g' config.h
-sed -i 's/HAVE_TRUNC 0/HAVE_TRUNC 1/g' config.h
-sed -i 's/HAVE_TRUNCF 0/HAVE_TRUNCF 1/g' config.h
-
 cd ..
